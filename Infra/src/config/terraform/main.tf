406ce351aa717aef011d89565434969fb1d547df
Stas Miasnikov <mias@M5.ad.morphisec.com>
2019-10-13 14:23:26 +0300

    Automatic commit

--- Infra/src/config/terraform/main.tf
+++ Infra/src/config/terraform/main.tf
@@ -1,5 +1,15 @@
 provider "aws" {
   region = "us-west-2"
-  shared_credentials_file = "creds"
+  shared_credentials_file = "c:\\creds"
   profile = "learning"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "stas-learning-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
